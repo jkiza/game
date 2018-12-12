41aaff3ac03b1303732c0a0a34f78785420d2f1c
Izabela Konkiel <k.izabela97@icloud.com>
2018-12-12 14:08:49 +0000

    s

--- iz/js/game.js
+++ iz/js/game.js
@@ -220,7 +220,7 @@ gameScene.create = function () {
         fill: '#ffffff'
     });
     
-    var mute = this.add.text(50, 980, 'Mute', {
+    var mute = this.add.text(500, 980, 'Mute', {
         font: '32px monospace',
         fill: '#ffffff'
     });
